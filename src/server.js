d10f32794e818ad2cbd6674e01381f70aed97673
Rizki Ardianto <Rizki.Ardianto@indonesia.hirose.co.id>
2026-01-21 15:39:22 +0700

    change port be

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 const app = require('./app')
 const sequelize = require('./config/database')
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 (
   async () => {
