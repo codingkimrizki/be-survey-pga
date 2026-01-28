ee466e3c93cfe0529de7a7cc7b7c079f1e728959
Rizki Ardianto <Rizki.Ardianto@indonesia.hirose.co.id>
2026-01-28 14:23:04 +0700

    migrate and seed

--- migrate.js
+++ migrate.js
@@ -3,13 +3,32 @@ const { spawnSync } = require('child_process');
 
 console.log('🚀 Running migrations...');
 
-const result = spawnSync('node', ['node_modules/sequelize-cli/lib/sequelize', 'db:migrate'], {
-  stdio: 'inherit'
-});
+// Jalankan migrate dulu
+const migrate = spawnSync(
+  'node',
+  ['node_modules/sequelize-cli/lib/sequelize', 'db:migrate'],
+  { stdio: 'inherit' }
+);
 
-if (result.status !== 0) {
+if (migrate.status !== 0) {
   console.error('❌ Migration failed');
-  process.exit(result.status);
+  process.exit(migrate.status);
 }
 
 console.log('✅ Migrations done!');
+
+// Jalankan seed setelah migrate sukses
+console.log('🚀 Running seeds...');
+
+const seed = spawnSync(
+  'node',
+  ['node_modules/sequelize-cli/lib/sequelize', 'db:seed:all'],
+  { stdio: 'inherit' }
+);
+
+if (seed.status !== 0) {
+  console.error('❌ Seeding failed');
+  process.exit(seed.status);
+}
+
+console.log('✅ Seeding done!');
