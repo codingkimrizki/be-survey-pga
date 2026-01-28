0c38e7b81357b96045216959f5e1d48d1500a0b1
Rizki Ardianto <Rizki.Ardianto@indonesia.hirose.co.id>
2026-01-28 12:00:08 +0700

    edit9

--- migrate.js
+++ migrate.js
@@ -3,7 +3,7 @@ const { execSync } = require("child_process");
 
 try {
   console.log("🚀 Running migrations...");
-  execSync("node_modules/.bin/sequelize-cli db:migrate", { stdio: "inherit", shell: true });
+  execSync("npx sequelize-cli db:migrate", { stdio: "inherit", shell: true });
 
   // console.log("🌱 Running seeds...");
   // execSync("node_modules/.bin/sequelize-cli db:seed:all", { stdio: "inherit", shell: true });
